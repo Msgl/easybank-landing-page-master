f41f4b7e2b0d7b55e5f155088fc08f7c26cd6bda
Msgl <mariasglynou@gmail.com>
2021-03-22 23:23:45 +0200

    Mobile Layout

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const toggleNav = () => {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active');
+    // const sectionIntro = document.querySelectorAll('.section-intro');
+}
+
+
